d6494ca6fd881af8c4bdfaf09324994a25c7f2c7
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-12-01 19:34:13 +0800

    Initial commit

---
+++ adventofcode/day1/day1.py
@@ -0,0 +1,23 @@
+input_ = list(map(int, open('input.txt').read().splitlines()))
+
+
+def part1(inp: list[int]) -> None:
+    for i in range(len(inp)):
+        for j in range(i + 1, len(inp)):
+            if i != j and inp[i] + inp[j] == 2020:
+                print(inp[i] * inp[j])
+                return
+
+
+def part2(inp: list[int]) -> None:
+    for i in range(len(inp)):
+        for j in range(i+1, len(inp)):
+            for k in range(j+1, len(inp)):
+                if len({i, j, k}) == 3 and inp[i] + inp[j] + inp[k] == 2020:
+                    print(inp[i] * inp[j] * inp[k])
+                    return
+
+
+if __name__ == '__main__':
+    part1(input_)
+    part2(input_)
