bd6a8893ac349ec8465f19129391076d77796cb9
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-12-18 04:05:06 +0800

    Add stub for future days

---
+++ day24/day24.py
@@ -0,0 +1,17 @@
+INPUT = open('input.txt').read().splitlines()
+TEST = open('test.txt').read().splitlines()
+
+
+def part1(inp: list[str]) -> None:
+    pass
+
+
+def part2(inp: list[str]) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    part1(TEST)
+    part2(TEST)
+    part1(INPUT)
+    part2(INPUT)
